92081c665f7b7fba8ec9eea022b43aa82230cb58
Brunobk2c <bk2c@hotmail.com>
2023-06-25 22:39:57 +0100

    Finalize backend

--- Api 2 Original/API/src/routes/Routes.js
+++ Api 2 Original/API/src/routes/Routes.js
@@ -3,72 +3,20 @@ const router  = express.Router();
 const auth = require('../middlewares/auth');
 const passport = require("passport");
 const AuthController = require('../../controllers/AuthController');
-const app = express()
-
-const { Artista } = require('../../db/models/artista')
+const app = express();
 
 // ---------- MusicasController ----------
+const musicas = require('../../controllers/musicas')
+app.use('/', musicas)
+
 // ---------- ArtistasController ----------
-const artista = require('../../controllers/artista')
-app.use('/', artista)
-
-router.get('/artistas', async (req, res) => {
-    const artista = await Artista.findAll();
-    res.json(artista);
-});
-
-
-router.post('/artistas', async (req, res) => {
-    const { name, email, createAt, updateAt} = req.body;
-    const artista = await Artista.create({ name, email, createAt, updateAt });
-    res.json(artista);
-});
-
-
-
-router.get('/artistas/{id}', async (req, res) => {
-    const { id } = req.body;
-    console.log(id)
-    const artista = await Artista.findByPk();
-    res.json(artista);
-  
-    // if (!artista) {
-    //   return res.status(404).json({ error: 'Artista not found' });
-    // }
-  
-    // res.json(artista);
-});
-  
-router.put('/artistas/{id}', async (req, res) => {
-    const { id } = req.body.id;
-    const { name, email } = req.body;
-    const artista = await Artista.findByPk(id);
-  
-    if (!artista) {
-      return res.status(404).json({ error: 'Artista not found' });
-    }
-  
-    artista.name = name;
-    artista.email = email;
-    await artista.save();
-  
-    res.json(artista);
- });
-  
-router.delete('/artistas/:id', async (req, res) => {
-    const { id } = req.params;
-    const artista = await Artista.findByPk(id);
-  
-    if (!artista) {
-      return res.status(404).json({ error: 'Artista not found' });
-    }
-  
-    await artista.destroy();
-  
-    res.status(204).send();
-  });
+const artista = require('../../controllers/artista');
+app.use('/', artista);
 
 // ---------- AlbumController ----------
+const album = require('../../controllers/album')
+app.use('/', album)
+
 // ---------- AlbumporArtistaController ----------
 // ---------- MusicasporArtistaController ----------
 // ---------- MusicasporGeneroController ----------
